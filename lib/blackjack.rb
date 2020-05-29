f3f983d65a978a468ba6a6cda39e9d7256903dce
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-29 17:46:39 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,10 +5,12 @@ end
 
 def deal_card
   # code #deal_card here
+  random num between 1-11, returns num
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Total is ${total}"
 end
 
 def prompt_user
